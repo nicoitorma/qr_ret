43bce2fc8ae6148227ee23678408485ab6d94f8a
Nico-Itorma <nicoitorma38@gmail.com>
2023-03-24 22:12:09 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
-  String? idNum, course, year;
+  String? idNum;
+  String dropdownValue = 'Program';
+  String? value1;
   TextEditingController text1 = TextEditingController();
   TextEditingController text2 = TextEditingController();
-  TextEditingController text3 = TextEditingController();
+
+  Future<Uint8List> loadImageFromAssets(String path) async {
+    ByteData imageData = await rootBundle.load(path);
+    return imageData.buffer.asUint8List();
+  }
+
+  showQR() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: FutureBuilder(
+                  future: loadImageFromAssets('assets/$value1/$idNum.png'),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      Uint8List bytes = snapshot.data as Uint8List;
+                      return Image.memory(bytes);
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  }),
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,73 +66,64 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: TextFormField(
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your Course/Program';
-                              }
-                              return null;
-                            },
-                            controller: text1,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Enter Program',
-                              labelText: 'Program',
-                            ),
-                            inputFormatters: [
-                              FilteringTextInputFormatter.allow(RegExp("[BSCITbscit]")),
-                            ]),
-                      ),
-                      TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                          controller: text2,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Enter ID Number',
-                            labelText: 'ID Number',
-                          ),
-                          inputFormatters: [
-                            FilteringTextInputFormatter.allow(RegExp("[0-9-]")),
-                          ]),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            idNum = text2.text;
-                            setState(() {});
-                          }
-                        },
-                        child: const Text('Submit'),
-                      ),
-                    ]),
-              ),
-            ),
-            idNum == null
-                ? Text('NULL')
-                : Image.asset(
-                    'assets/BSCS3/${idNum}.png',
-                    width: 200,
-                    height: 200,
+        child: Column(children: [
+          const SizedBox(height: 30),
+          const Text('CICT QR Repository'),
+          const SizedBox(height: 30),
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your ID Number';
+                    }
+                    return null;
+                  },
+                  controller: text2,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter ID Number',
+                    labelText: 'ID Number',
                   ),
-          ],
-        ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp("[0-9-]")),
+                  ]),
+            ),
+          ),
+          const SizedBox(height: 30),
+          DropdownButton<String>(
+            hint: const Text('Program'),
+            value: value1,
+            items: <String>['BSCS', 'BSInfo.Tech', 'BSIS']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(
+                  value,
+                  style: const TextStyle(fontSize: 18),
+                ),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                value1 = newValue!;
+              });
+            },
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                idNum = text2.text;
+                showQR();
+              }
+            },
+            child: const Text('Submit'),
+          ),
+        ]),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
